78c94668bd86ceece3b45f4f2d9ad002ba9fd958
Matias Juntunen <matias.juntunen@gmail.com>
2017-12-08 14:46:27 +0200

    Add stuff to precompiled header.

--- ff7gx/stdafx.h
+++ ff7gx/stdafx.h
@@ -8,13 +8,25 @@
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files:
-#include <windows.h>
 
-#include <d3d9.h>
+#include <DirectXMath.h>
+#include <Windows.h>
 
+#include <algorithm>
+#include <array>
+#include <assert.h>
 #include <cassert>
+#include <cmath>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <d3d9.h>
+#include <d3dcommon.h>
 #include <functional>
+#include <memory>
+#include <string>
+#include <type_traits>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include <wrl.h>
